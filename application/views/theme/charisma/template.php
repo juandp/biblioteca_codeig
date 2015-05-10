cfa61d257eab6419b01e6cd0af42b47798ea0eed
juandphotmail <jdperea59@mailinator.com>
2015-05-10 10:09:16 -0500

    Base del template completa

--- application/views/theme/charisma/template.php
+++ application/views/theme/charisma/template.php
@@ -4,6 +4,18 @@
         <?PHP $this -> load -> view(THEME.'head') ?>
     </head>
     <body>
+        <!-- START TOPBAR -->
         <?PHP $this -> load -> view(THEME.'topbar') ?>
+        <!-- END TOPBAR -->
+        <div class="ch-container">
+            <div class="row">
+                <?PHP $this -> load -> view(THEME.'left_menu') ?>
+                <!-- content  -->
+                <div id="content" class="col-lg-10 col-sm-10">
+                </div>
+                <!-- end content -->
+                
+            </div>
+        </div>
     </body>
 </html>
