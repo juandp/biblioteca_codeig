af8d36b8bff40705e0369bc7dd5c19ab43013f66
juandphotmail <jdperea59@mailinator.com>
2015-05-10 13:00:01 -0500

    Se agregan las migas de pan a el template

---
+++ application/views/theme/charisma/breadcrumb.php
@@ -0,0 +1,11 @@
+
+<div>
+    <ul class="breadcrumb">
+        <li>
+            <a href="#">Home</a>
+        </li>
+        <li>
+            <a href="#">Dashboard</a>
+        </li>
+    </ul>
+</div>
